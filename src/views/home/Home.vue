2aabf18c55e3fb70b9229abea0e9a2b78b6dab82
wyftddev <wyhope2018@163.com>
2020-05-04 22:20:36 +0800

    home页面feature组件封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,11 @@
 <template>
-    <div>
+    <div id="home">
         <nav-bar class="home-nav">
             <div slot="center">首页</div>
         </nav-bar>
         <home-swiper :banners="banner"></home-swiper>
         <recommend-view :recommends="recommend"></recommend-view>
+        <feature></feature>
     </div>
 </template>
 
@@ -12,6 +13,7 @@
     import NavBar from "components/common/navbar/NavBar"
     import HomeSwiper from "./childrenComponents/HomeSwiper";
     import RecommendView from "./childrenComponents/RecommendView";
+    import Feature from "./childrenComponents/Feature";
 
     import {getHomeMultiData} from "network/home";
 
@@ -27,7 +29,8 @@
         components: {
             NavBar,
             HomeSwiper,
-            RecommendView
+            RecommendView,
+            Feature
         },
         created() {
             getHomeMultiData()
@@ -44,8 +47,19 @@
 </script>
 
 <style scoped>
+    #home {
+        padding-top: 44px;
+    }
+
     .home-nav {
         background-color: var(--color-tint);
         color: white;
+
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 10;
+
     }
 </style>
